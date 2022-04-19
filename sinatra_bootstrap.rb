d0fd8c5a1ffa46cdafbcf65c601f1529d225adc0
Kostya Sigarov <ksigaryov@gmail.com>
2022-04-19 15:51:31 +0300

    Added page views/message.erb; added @title, @message into file sinatra_bootstrap.rb

--- sinatra_bootstrap.rb
+++ sinatra_bootstrap.rb
@@ -3,14 +3,17 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
+  @title = "Barbershop"
   erb "Greeting you in our barbershop!"
 end
 
 get '/about' do
+  @title = "О нас"
   erb :about
 end
 
 get '/contacts' do
+  @title = 'Контакты'
   erb :contacts
 end
 
@@ -18,7 +21,12 @@ post '/contacts' do
         @user_email = params[:user_email] # получить, то что отправил браузер
         @user_message = params[:user_message] # получить, то что отправил браузер
 
+        @title = 'Контакты'
+        @message = 'Информация успешно отправлена!'
+
         output = File.open('./public/contacts.txt', 'a')
-        output.write "User: #{@user_email}, message: #{@user_message}\n"
+        output.write "User email: #{@user_email}, user message: #{@user_message}\n"
         output.close
+
+        erb :message
 end
