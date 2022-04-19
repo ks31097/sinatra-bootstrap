5beead866edc02c45c2e8354cb063dee33c1b394
Kostya Sigarov <ksigaryov@gmail.com>
2022-04-19 17:17:24 +0300

    Added jQuery color picker

--- sinatra_bootstrap.rb
+++ sinatra_bootstrap.rb
@@ -41,13 +41,14 @@ post '/visit' do
         @user_phone = params[:user_phone] # получить, то что отправил браузер со страницы visit.erb <input name="user_phone" type="text" class="form-control" placeholder="Номер Вашего телефона">
         @date_time = params[:date_time] # получить, то что отправил браузер со страницы visit.erb <input name="date_time" type="text" class="form-control" placeholder="Введите дату и время">
         @master = params[:master] # получить, то что отправил браузер со страницы visit.erb <select name="master" class="form-select" aria-label="Default select example">
+        @color = params[:color] # получить, то что отправил браузер со страницы visit.erb <select id="color" name="color">
 
         @title = 'Записаться'
         @info = 'Вы записались!'
-        @message = "#{@user_name}, мы Вас ждем #{@date_time}. Ваш мастер #{@master}!"
+        @message = "#{@user_name}, мы Вас ждем #{@date_time}. Ваш мастер #{@master}. Цвет окраски волос: #{@color}!"
 
         output = File.open('./public/visits.txt', 'a')
-        output.write "User: #{@user_name}, Phone: #{@user_phone}, Date and time: #{@date_time}, master: #{@master}\n"
+        output.write "User: #{@user_name}, Phone: #{@user_phone}, Date and time: #{@date_time}, master: #{@master}, color: #{@color}\n"
         output.close
 
         erb :message
