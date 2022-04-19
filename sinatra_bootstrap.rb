77a22cade6f6ab041f437004760229c26f340328
Kostya Sigarov <ksigaryov@gmail.com>
2022-04-19 14:02:35 +0300

    Added page views/contacts.erb; added in sinatra_bootstrap.rb get and post method for contacts.erb

--- sinatra_bootstrap.rb
+++ sinatra_bootstrap.rb
@@ -9,3 +9,16 @@ end
 get '/about' do
   erb :about
 end
+
+get '/contacts' do
+  erb :contacts
+end
+
+post '/contacts' do
+        @user_email = params[:user_email] # получить, то что отправил браузер
+        @user_message = params[:user_message] # получить, то что отправил браузер
+
+        output = File.open('./public/contacts.txt', 'a')
+        output.write "User: #{@user_email}, message: #{@user_message}\n"
+        output.close
+end
